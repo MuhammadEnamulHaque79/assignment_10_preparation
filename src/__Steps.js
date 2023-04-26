3d899cb64d6ef5b81de19d39d180cbe652c13a35
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2023-04-26 23:32:46 +0600

    first commit

---
+++ src/__Steps.js
@@ -0,0 +1,5 @@
+/*
+1.create app in firebase;
+2.register app;
+3.npm install firebase
+*/
---
+++ src/firbase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzJK7aeTzXVb9s4h3JazOJ0SIXlmCJMzY",
+  authDomain: "assignment-10-preparation.firebaseapp.com",
+  projectId: "assignment-10-preparation",
+  storageBucket: "assignment-10-preparation.appspot.com",
+  messagingSenderId: "250126324650",
+  appId: "1:250126324650:web:5c621da89871cce8f591d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
